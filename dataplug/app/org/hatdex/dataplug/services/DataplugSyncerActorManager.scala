7be60e7420f9a5e01fdee2ad012ca3f2e037ca28
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2017-05-24 11:19:28 +0100

    Adds 10s delay between individual HAT sync initialisations

--- dataplug/app/org/hatdex/dataplug/services/DataplugSyncerActorManager.scala
+++ dataplug/app/org/hatdex/dataplug/services/DataplugSyncerActorManager.scala
@@ -10,7 +10,10 @@ package org.hatdex.dataplug.services
 
 import javax.inject.{ Inject, Named }
 
+import akka.{ Done, NotUsed }
 import akka.actor.ActorRef
+import akka.stream.{ Materializer, ThrottleMode }
+import akka.stream.scaladsl.Source
 import com.mohiva.play.silhouette.api.{ LoginInfo, Provider }
 import com.mohiva.play.silhouette.api.services.IdentityService
 import com.mohiva.play.silhouette.impl.providers.{ CommonSocialProfile, SocialProvider, SocialProviderRegistry }
@@ -21,12 +24,16 @@ import org.hatdex.dataplug.models.User
 import play.api.Logger
 
 import scala.concurrent.{ ExecutionContext, Future }
+import scala.concurrent.duration._
 
 class DataplugSyncerActorManager @Inject() (
     socialProviderRegistry: SocialProviderRegistry,
     dataPlugEndpointService: DataPlugEndpointService,
     optionsCollectionRegistry: DataPlugOptionsCollectorRegistry,
+    implicit val materializer: Materializer,
     @Named("dataPlugManager") dataPlugManagerActor: ActorRef) {
+
+  private val logger = Logger("SyncerActorManager")
 
   def updateApiVariantChoices(user: User, variantChoices: Seq[ApiEndpointVariantChoice])(implicit ec: ExecutionContext): Future[Unit] = {
     dataPlugEndpointService.updateApiVariantChoices(user.userId, variantChoices) map {
@@ -42,14 +49,18 @@ class DataplugSyncerActorManager @Inject() (
     }
   }
 
-  def startAllActiveVariantChoices()(implicit ec: ExecutionContext): Future[Unit] = {
+  def startAllActiveVariantChoices()(implicit ec: ExecutionContext): Future[Done] = {
     Logger.info("Starting active API endpoint syncing")
-    dataPlugEndpointService.retrieveAllEndpoints map { phataVariants =>
+    dataPlugEndpointService.retrieveAllEndpoints flatMap { phataVariants =>
       Logger.info(s"Retrieved endpoints to sync: ${phataVariants.mkString("\n")}")
-      phataVariants foreach {
-        case (phata, variant) =>
-          dataPlugManagerActor ! Start(variant, phata, variant.configuration)
-      }
+      Source.fromIterator(() => phataVariants.iterator)
+        .throttle(1, 10.seconds, 1, ThrottleMode.Shaping)
+        .map {
+          case (phata, variant) =>
+            dataPlugManagerActor ! Start(variant, phata, variant.configuration)
+            phata
+        }
+        .runForeach(r => logger.info(s"$r - initializing data sync"))
     } recoverWith {
       case e =>
         Logger.error(s"Could not retrieve endpoints to sync: ${e.getMessage}")
