d729038cfddcde7389a6c2e03855a7df41408d13
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2019-03-26 15:04:00 +0000

    Moves data migration evolution into a separate file

--- dataplug/conf/evolutions/1.sql
+++ dataplug/conf/evolutions/1.sql
@@ -141,20 +141,6 @@ CREATE TABLE dataplug_user_status (
   message                VARCHAR
 );
 
-INSERT INTO dataplug_user_status (phata, dataplug_endpoint, endpoint_configuration, endpoint_variant, created, successful, message)
-SELECT s.phata, s.dataplug_endpoint, s.endpoint_configuration, s.endpoint_variant, s.created, s.successful, s.message
-FROM (
-       SELECT phata, dataplug_endpoint, endpoint_variant, MAX(created) AS MaxDate
-       FROM log_dataplug_user
-       GROUP BY phata, dataplug_endpoint, endpoint_variant
-     ) m
-      INNER JOIN log_dataplug_user s
-        ON m.phata = s.phata
-          AND m.dataplug_endpoint = s.dataplug_endpoint
-          AND m.endpoint_variant = s.endpoint_variant
-          AND m.MaxDate = s.created;
-
---rollback DROP TABLE log_dataplug_user_status;
---rollback DROP SEQUENCE log_dataplug_status_seq_id;
-
+--rollback DROP TABLE dataplug_user_status;
+--rollback DROP SEQUENCE dataplug_user_status_seq_id;
 
