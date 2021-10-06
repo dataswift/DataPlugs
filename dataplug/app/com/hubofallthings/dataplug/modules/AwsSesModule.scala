0b983dff113e716b6ca7fe87ecc2115c58a219dd
Piotr <piotr.perzyna@gmail.com>
2021-10-06 18:18:20 +0200

    update aws provider

--- dataplug/app/com/hubofallthings/dataplug/modules/AwsSesModule.scala
+++ dataplug/app/com/hubofallthings/dataplug/modules/AwsSesModule.scala
@@ -1,12 +1,10 @@
 package com.hubofallthings.dataplug.modules
 
-import com.amazonaws.auth.EC2ContainerCredentialsProviderWrapper
-import com.amazonaws.services.simpleemail.{AmazonSimpleEmailService, AmazonSimpleEmailServiceClientBuilder}
+import com.amazonaws.services.simpleemail.{ AmazonSimpleEmailService, AmazonSimpleEmailServiceClientBuilder }
 import com.google.inject.Provides
 import net.codingwell.scalaguice.ScalaModule
-import play.api.Configuration
 
-import javax.inject.{Singleton => JSingleton}
+import javax.inject.{ Singleton => JSingleton }
 
 class AwsSesModule extends ScalaModule {
 
@@ -14,11 +12,6 @@ class AwsSesModule extends ScalaModule {
 
   @Provides
   @JSingleton
-  def provideAwsEmailService(config: Configuration): AmazonSimpleEmailService =
-    AmazonSimpleEmailServiceClientBuilder
-      .standard()
-      .withRegion(config.get[String]("mailer.awsRegion"))
-      .withCredentials(new EC2ContainerCredentialsProviderWrapper)
-      .build()
+  def provideAwsEmailService: AmazonSimpleEmailService = AmazonSimpleEmailServiceClientBuilder.defaultClient()
 
 }
